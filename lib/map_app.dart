25b15cbc8f2d1fe7fc7719e96760e44eddf0f639
Pooria Askari Moqaddam <pooriaskarim@gmail.com>
2025-09-06 10:41:09 +0330

    RTL support and dynamic theme switching added. Fonts switched to IRANSans.

--- lib/map_app.dart
+++ lib/map_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'view/common/controllers/controller.app.dart';
 import 'view/common/routes/routes.dart';
 import 'view/screens/splash/bindings/bindings.splash.dart';
 import 'view/screens/splash/view/screen.splash.dart';
@@ -9,23 +10,28 @@ class MapApp extends StatelessWidget {
   const MapApp({super.key});
 
   @override
-  Widget build(final BuildContext context) => GetMaterialApp(
-    getPages: Routes.routes,
-    home: const SplashScreen(),
-    initialBinding: SplashBindings(),
-    title: 'Map App',
-    theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-      fontFamily: 'Roboto',
-    ),
+  Widget build(final BuildContext context) => Obx(
+    () => Directionality(
+      textDirection: TextDirection.rtl,
+      child: GetMaterialApp(
+        getPages: Routes.routes,
+        home: const SplashScreen(),
+        initialBinding: SplashBindings(),
+        title: 'Map App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+          fontFamily: 'IRANSans',
+        ),
 
-    darkTheme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.indigo,
-        brightness: Brightness.dark,
+        darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.indigo,
+            brightness: Brightness.dark,
+          ),
+          fontFamily: 'IRANSans',
+        ),
+        themeMode: Get.find<AppController>().themeMode.value,
       ),
-      fontFamily: 'Roboto',
     ),
-    themeMode: ThemeMode.light,
   );
 }
